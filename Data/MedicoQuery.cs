4de0856823da07f70906aa52d809005c27921ee6
rpcorralesvargas-maker <rpcorralesvargas@gmail.com>
2025-08-20 21:32:46 -0600

    Add files via upload

---
+++ Data/MedicoQuery.cs
@@ -0,0 +1,154 @@
+﻿using System.Data;
+using Microsoft.IdentityModel.Tokens;
+using MySql.Data.MySqlClient;
+using Proyecto_Final.Modelo;
+
+namespace Proyecto_Final.Data
+{
+    public class MedicoQuery
+    {
+        List<Medico> medicos;
+        ConexionBD conexionBD;
+
+        public MedicoQuery()
+        {
+            medicos = new List<Medico>();
+            conexionBD = new ConexionBD();
+        }
+
+        public List<Medico> GetMedicos()
+        {
+            List<Medico> medicos = new List<Medico>();
+            MySqlConnection conexion = conexionBD.AbrirConexion();
+
+            MySqlCommand comando = new MySqlCommand("obtenerTodosLosMedicos", conexion);
+            comando.CommandType = CommandType.StoredProcedure;
+
+            MySqlDataReader lector = comando.ExecuteReader();
+
+            while (lector.Read())
+            {
+                int id = Convert.ToInt32(lector["ID"]);
+                string nombre = lector["Nombre"].ToString();
+                string cedula = lector["Cedula"].ToString();
+                string especialidad = lector["Especialidad"].ToString();
+                int telefono = Convert.ToInt32(lector["Telefono"]);
+                string correo = lector["Correo"].ToString();
+                DateTime horario_consulta = Convert.ToDateTime(lector["Horario_consulta"]);
+                int estado = Convert.ToInt32(lector["Estado"]);
+
+                Medico medico = new Medico(id, nombre, cedula, especialidad, telefono, correo, horario_consulta, estado);
+                medicos.Add(medico);
+            }
+
+            lector.Close();
+            conexionBD.CerrarConexion(conexion);
+            return medicos;
+        }
+
+        public void AgregarMedico(Medico medico)
+        {
+            MySqlConnection conexion = conexionBD.AbrirConexion();
+
+            MySqlCommand comando = new MySqlCommand("insertMedico", conexion);
+            comando.CommandType = CommandType.StoredProcedure;
+
+            comando.Parameters.AddWithValue("p_Nombre", medico.Nombre);
+            comando.Parameters.AddWithValue("p_Cedula", medico.Cedula);
+            comando.Parameters.AddWithValue("p_Especialidad", medico.Especialidad);
+            comando.Parameters.AddWithValue("p_Telefono", medico.Telefono);
+            comando.Parameters.AddWithValue("p_Correo", medico.Correo);
+            comando.Parameters.AddWithValue("p_Horario_consulta", medico.Horario_consulta);
+            comando.Parameters.AddWithValue("p_Estado", medico.Estado);
+
+            comando.ExecuteNonQuery();
+            conexionBD.CerrarConexion(conexion);
+        }
+
+        public Medico BuscarMedico(int id)
+        {
+            MySqlConnection conexion = conexionBD.AbrirConexion();
+
+            MySqlCommand comando = new MySqlCommand("consultarMedicoPorID", conexion);
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("p_id", id);
+
+            MySqlDataReader lector = comando.ExecuteReader();
+            lector.Read();
+
+            Medico medico = new Medico(
+                Convert.ToInt32(lector["ID"]),
+                lector["Nombre"].ToString(),
+                lector["Cedula"].ToString(),
+                lector["Especialidad"].ToString(),
+                Convert.ToInt32(lector["Telefono"]),
+                lector["Correo"].ToString(),
+                Convert.ToDateTime(lector["Horario_consulta"]),
+                Convert.ToInt32(lector["Estado"])
+            );
+
+            lector.Close();
+            conexionBD.CerrarConexion(conexion);
+            return medico;
+        }
+
+        public void ActualizarMedico(int id, Medico medico)
+        {
+            MySqlConnection conexion = conexionBD.AbrirConexion();
+
+            try
+            {
+                MySqlCommand comando = new MySqlCommand();
+                comando.Connection = conexion;
+
+                List<string> setClauses = new List<string>();
+
+                if (!string.IsNullOrEmpty(medico.Nombre)) setClauses.Add("Nombre = @Nombre");
+                if (!string.IsNullOrEmpty(medico.Cedula)) setClauses.Add("Cedula = @Cedula");
+                if (!string.IsNullOrEmpty(medico.Especialidad)) setClauses.Add("Especialidad = @Especialidad");
+                if (medico.Telefono.HasValue) setClauses.Add("Telefono = @Telefono");
+                if (!string.IsNullOrEmpty(medico.Correo)) setClauses.Add("Correo = @Correo");
+                if (medico.Horario_consulta.HasValue) setClauses.Add("Horario_consulta = @Horario_consulta");
+                if (medico.Estado.HasValue) setClauses.Add("Estado = @Estado");
+
+                if (setClauses.Count == 0)
+                {
+                    conexionBD.CerrarConexion(conexion);
+                    return; // No hay nada que actualizar
+                }
+
+                string query = $"UPDATE Medico SET {string.Join(", ", setClauses)} WHERE ID = @ID";
+                comando.CommandText = query;
+
+                comando.Parameters.AddWithValue("@ID", id);
+                if (!string.IsNullOrEmpty(medico.Nombre)) comando.Parameters.AddWithValue("@Nombre", medico.Nombre);
+                if (!string.IsNullOrEmpty(medico.Cedula)) comando.Parameters.AddWithValue("@Cedula", medico.Cedula);
+                if (!string.IsNullOrEmpty(medico.Especialidad)) comando.Parameters.AddWithValue("@Especialidad", medico.Especialidad);
+                if (medico.Telefono.HasValue) comando.Parameters.AddWithValue("@Telefono", medico.Telefono.Value);
+                if (!string.IsNullOrEmpty(medico.Correo)) comando.Parameters.AddWithValue("@Correo", medico.Correo);
+                if (medico.Horario_consulta.HasValue) comando.Parameters.AddWithValue("@Horario_consulta", medico.Horario_consulta.Value);
+                if (medico.Estado.HasValue) comando.Parameters.AddWithValue("@Estado", medico.Estado.Value);
+
+                comando.ExecuteNonQuery();
+            }
+            finally
+            {
+                conexionBD.CerrarConexion(conexion);
+            }
+        }
+
+
+
+        public void InactivarMedico(int id)
+        {
+            MySqlConnection conexion = conexionBD.AbrirConexion();
+
+            MySqlCommand comando = new MySqlCommand("eliminarMedicoPorID", conexion);
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("p_id", id);
+
+            comando.ExecuteNonQuery();
+            conexionBD.CerrarConexion(conexion);
+        }
+    }
+}
---
+++ Data/PacienteQuery.cs
@@ -0,0 +1,175 @@
+﻿using Proyecto_Final.Modelo;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Proyecto_Final.Data
+{
+    public class PacienteQuery
+    {
+        List<Paciente> pacientes;
+        ConexionBD conexionBD;
+
+        public PacienteQuery()
+        {
+            pacientes = new List<Paciente>();
+            conexionBD = new ConexionBD();
+        }
+
+        // Helper para valores nulos
+        private T GetValueOrDefault<T>(object value, T defaultValue)
+        {
+            return value != DBNull.Value ? (T)Convert.ChangeType(value, typeof(T)) : defaultValue;
+        }
+
+        public List<Paciente> GetPacientes()
+        {
+            List<Paciente> pacientes = new List<Paciente>();
+            MySqlConnection conexion = conexionBD.AbrirConexion();
+
+            MySqlCommand comando = new MySqlCommand("obtenerTodosLosPacientes", conexion);
+            comando.CommandType = CommandType.StoredProcedure;
+
+            MySqlDataReader lector = comando.ExecuteReader();
+
+            while (lector.Read())
+            {
+                int id = GetValueOrDefault<int>(lector["ID"], 0);
+                string nombre = GetValueOrDefault<string>(lector["Nombre"], "");
+                string cedula = GetValueOrDefault<string>(lector["Cedula"], "");
+                DateTime fecha_nacimiento = GetValueOrDefault<DateTime>(lector["Fecha_nacimiento"], DateTime.MinValue);
+                string genero = GetValueOrDefault<string>(lector["Genero"], "");
+                string direccion = GetValueOrDefault<string>(lector["Direccion"], "");
+                int telefono = GetValueOrDefault<int>(lector["Telefono"], 0);
+                string correo = GetValueOrDefault<string>(lector["Correo"], "");
+                int estado = GetValueOrDefault<int>(lector["Estado"], 0);
+                DateTime fecha_registro = GetValueOrDefault<DateTime>(lector["Fecha_registro"], DateTime.MinValue);
+
+                Paciente paciente = new Paciente(id, nombre, cedula, fecha_nacimiento, genero, direccion, telefono, correo, estado, fecha_registro);
+                pacientes.Add(paciente);
+            }
+
+            lector.Close();
+            conexionBD.CerrarConexion(conexion);
+            return pacientes;
+        }
+
+        public void AgregarPaciente(Paciente paciente)
+        {
+            MySqlConnection conexion = conexionBD.AbrirConexion();
+
+            MySqlCommand comando = new MySqlCommand("insertPaciente", conexion);
+            comando.CommandType = CommandType.StoredProcedure;
+
+            comando.Parameters.AddWithValue("p_Nombre", paciente.Nombre);
+            comando.Parameters.AddWithValue("p_Cedula", paciente.Cedula);
+            comando.Parameters.AddWithValue("p_Fecha_nacimiento", paciente.Fecha_nacimiento);
+            comando.Parameters.AddWithValue("p_Genero", paciente.Genero);
+            comando.Parameters.AddWithValue("p_Direccion", paciente.Direccion);
+            comando.Parameters.AddWithValue("p_Telefono", paciente.Telefono);
+            comando.Parameters.AddWithValue("p_Correo", paciente.Correo);
+            comando.Parameters.AddWithValue("p_Estado", paciente.Estado);
+            comando.Parameters.AddWithValue("p_Fecha_registro", paciente.Fecha_registro);
+
+            comando.ExecuteNonQuery();
+            conexionBD.CerrarConexion(conexion);
+        }
+
+        public Paciente BuscarPaciente(int id)
+        {
+            MySqlConnection conexion = conexionBD.AbrirConexion();
+
+            MySqlCommand comando = new MySqlCommand("consultarPacientePorID", conexion);
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("p_id", id);
+
+            MySqlDataReader lector = comando.ExecuteReader();
+
+            if (!lector.Read()) // Manejar si no hay resultados
+            {
+                lector.Close();
+                conexionBD.CerrarConexion(conexion);
+                return null;
+            }
+
+            Paciente paciente = new Paciente(
+                GetValueOrDefault<int>(lector["ID"], 0),
+                GetValueOrDefault<string>(lector["Nombre"], ""),
+                GetValueOrDefault<string>(lector["Cedula"], ""),
+                GetValueOrDefault<DateTime>(lector["Fecha_nacimiento"], DateTime.MinValue),
+                GetValueOrDefault<string>(lector["Genero"], ""),
+                GetValueOrDefault<string>(lector["Direccion"], ""),
+                GetValueOrDefault<int>(lector["Telefono"], 0),
+                GetValueOrDefault<string>(lector["Correo"], ""),
+                GetValueOrDefault<int>(lector["Estado"], 0),
+                GetValueOrDefault<DateTime>(lector["Fecha_registro"], DateTime.MinValue)
+            );
+
+            lector.Close();
+            conexionBD.CerrarConexion(conexion);
+            return paciente;
+        }
+
+        public void ActualizarPaciente(int id, Paciente paciente)
+        {
+            MySqlConnection conexion = conexionBD.AbrirConexion();
+
+            try
+            {
+                MySqlCommand comando = new MySqlCommand();
+                comando.Connection = conexion;
+
+                List<string> setClauses = new List<string>();
+
+                if (!string.IsNullOrEmpty(paciente.Nombre)) setClauses.Add("Nombre = @Nombre");
+                if (!string.IsNullOrEmpty(paciente.Cedula)) setClauses.Add("Cedula = @Cedula");
+                if (paciente.Fecha_nacimiento.HasValue) setClauses.Add("Fecha_nacimiento = @Fecha_nacimiento");
+                if (!string.IsNullOrEmpty(paciente.Genero)) setClauses.Add("Genero = @Genero");
+                if (!string.IsNullOrEmpty(paciente.Direccion)) setClauses.Add("Direccion = @Direccion");
+                if (paciente.Telefono != 0) setClauses.Add("Telefono = @Telefono");
+                if (!string.IsNullOrEmpty(paciente.Correo)) setClauses.Add("Correo = @Correo");
+                if (paciente.Estado != 0) setClauses.Add("Estado = @Estado");
+                if (paciente.Fecha_registro.HasValue) setClauses.Add("Fecha_registro = @Fecha_registro");
+
+                if (setClauses.Count == 0)
+                {
+                    conexionBD.CerrarConexion(conexion);
+                    return; // No hay nada que actualizar
+                }
+
+                string query = $"UPDATE Paciente SET {string.Join(", ", setClauses)} WHERE ID = @ID";
+                comando.CommandText = query;
+
+                comando.Parameters.AddWithValue("@ID", id);
+                if (!string.IsNullOrEmpty(paciente.Nombre)) comando.Parameters.AddWithValue("@Nombre", paciente.Nombre);
+                if (!string.IsNullOrEmpty(paciente.Cedula)) comando.Parameters.AddWithValue("@Cedula", paciente.Cedula);
+                if (paciente.Fecha_nacimiento.HasValue) comando.Parameters.AddWithValue("@Fecha_nacimiento", paciente.Fecha_nacimiento.Value);
+                if (!string.IsNullOrEmpty(paciente.Genero)) comando.Parameters.AddWithValue("@Genero", paciente.Genero);
+                if (!string.IsNullOrEmpty(paciente.Direccion)) comando.Parameters.AddWithValue("@Direccion", paciente.Direccion);
+                if (paciente.Telefono != 0) comando.Parameters.AddWithValue("@Telefono", paciente.Telefono);
+                if (!string.IsNullOrEmpty(paciente.Correo)) comando.Parameters.AddWithValue("@Correo", paciente.Correo);
+                if (paciente.Estado != 0) comando.Parameters.AddWithValue("@Estado", paciente.Estado);
+                if (paciente.Fecha_registro.HasValue) comando.Parameters.AddWithValue("@Fecha_registro", paciente.Fecha_registro.Value);
+
+                comando.ExecuteNonQuery();
+            }
+            finally
+            {
+                conexionBD.CerrarConexion(conexion);
+            }
+        }
+
+
+
+        public void InactivarPaciente(int id)
+        {
+            MySqlConnection conexion = conexionBD.AbrirConexion();
+
+            MySqlCommand comando = new MySqlCommand("eliminarPacientePorID", conexion);
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("p_id", id);
+
+            comando.ExecuteNonQuery();
+            conexionBD.CerrarConexion(conexion);
+        }
+    }
+}
