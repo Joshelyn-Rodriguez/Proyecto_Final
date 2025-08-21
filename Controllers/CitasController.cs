b9133bd2fac9d715b949f4aa93a8fc18c3a490f9
Joshelyn-Rodriguez <jarodriguezc_est@ucatolica.ac.cr>
2025-08-20 21:27:09 -0600

    Add files via upload

---
+++ Controllers/CitasController.cs
@@ -0,0 +1,75 @@
+﻿
+
+using Proyecto_Final.Data;
+using Proyecto_Final.Modelo;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Collections.Generic;
+using System.IO;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Proyecto_Final.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitasController : ControllerBase
+    {
+
+        CitasQuery citQuery;
+
+        public CitasController()
+        {
+            citQuery = new CitasQuery();
+        }
+
+
+        // GET: api/<CitasController>
+        [HttpGet]
+        public List<Cita> Get()
+        {
+            List<Cita> citas = citQuery.GetCitas();
+            return citas;
+        }
+
+
+        // GET api/<CitasController>/5
+
+        [HttpGet("{id}")]
+        public Cita Get(int id)
+        {
+            Cita cita = citQuery.BuscarCita(id);
+            return cita;
+        }
+
+
+        // POST api/<CitasController>
+        [HttpPost]
+        public void Post([FromBody] Cita cita)
+        {
+            citQuery.AgregarCita(cita);
+        }
+
+        // PUT api/<CitasController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Cita cita)
+        {
+            citQuery.ActualizarCita(id, cita);
+        }
+
+        // DELETE api/<CitasController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            citQuery.InactivarCita(id);
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Controllers/HistorialController.cs
@@ -0,0 +1,54 @@
+﻿using Proyecto_Final.Data;
+using Proyecto_Final.Modelo;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Proyecto_Final.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HistorialController : ControllerBase
+    {
+        private HistorialQuery hisQuery;
+
+        public HistorialController()
+        {
+            hisQuery = new HistorialQuery();
+        }
+
+        // GET: api/<HistorialController>
+        [HttpGet]
+        public List<Historial> Get()
+        {
+            return hisQuery.GetHistoriales();
+        }
+
+        // GET api/<HistorialController>/5
+        [HttpGet("{id}")]
+        public Historial Get(int id)
+        {
+            return hisQuery.BuscarHistorial(id);
+        }
+
+        // POST api/<HistorialController>
+        [HttpPost]
+        public void Post([FromBody] Historial historial)
+        {
+            hisQuery.AgregarHistorial(historial);
+        }
+
+        // PUT api/<HistorialController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Historial historial)
+        {
+            hisQuery.ActualizarHistorial(id, historial);
+        }
+
+        // DELETE api/<HistorialController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            hisQuery.EliminarHistorial(id);
+        }
+    }
+}
