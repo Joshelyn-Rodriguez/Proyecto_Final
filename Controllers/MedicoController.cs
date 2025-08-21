a38ba739f784b23748020f2d6d59ebd1eb7810af
rpcorralesvargas-maker <rpcorralesvargas@gmail.com>
2025-08-20 21:31:42 -0600

    Add files via upload

---
+++ Controllers/MedicoController.cs
@@ -0,0 +1,69 @@
+﻿
+
+using Proyecto_Final.Data;
+using Proyecto_Final.Modelo;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Collections.Generic;
+using System.IO;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Proyecto_Final.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MedicoController : ControllerBase
+    {
+
+        MedicoQuery medQuery;
+
+        public MedicoController()
+        {
+            medQuery = new MedicoQuery();
+        }
+
+
+        // GET: api/<MedicoController>
+        [HttpGet]
+        public List<Medico> Get()
+        {
+            List<Medico> medicos = medQuery.GetMedicos();
+            return medicos;
+        }
+
+
+        // GET api/<MedicoController>/5
+
+        [HttpGet("{id}")]
+        public Medico Get(int id)
+        {
+            Medico medico = medQuery.BuscarMedico(id);
+            return medico;
+        }
+
+
+        // POST api/<MedicoController>
+        [HttpPost]
+        public void Post([FromBody] Medico medico)
+        {
+            medQuery.AgregarMedico(medico);
+        }
+
+        // PUT api/<MedicoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Medico medico)
+        {
+            medQuery.ActualizarMedico(id, medico);
+        }
+
+        // DELETE api/<MedicoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            medQuery.InactivarMedico(id);
+        }
+    }
+}
---
+++ Controllers/PacienteController.cs
@@ -0,0 +1,67 @@
+﻿
+using Proyecto_Final.Autenticación;
+using Proyecto_Final.Data;
+using Proyecto_Final.Modelo;
+using Microsoft.AspNetCore.Mvc;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Proyecto_Final.Controllers
+{
+
+    [BasicAuthentication]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PacienteController : ControllerBase
+    {
+
+        PacienteQuery pacQuery;
+
+        public PacienteController( )
+        {
+            pacQuery = new PacienteQuery();
+        }
+
+
+        // GET: api/<PacienteController>
+        [HttpGet]
+        public List<Paciente> Get()
+        {
+            List<Paciente> pacientes = pacQuery.GetPacientes();
+            return pacientes;
+        }
+
+
+        // GET api/<PacienteController>/5
+
+        [HttpGet("{id}")]
+        public Paciente Get(int id)
+        {
+            Paciente paciente = pacQuery.BuscarPaciente(id);
+            return paciente;
+        }
+
+
+        // POST api/<PacienteController>
+        [HttpPost]
+        public void Post([FromBody] Paciente paciente)
+        {
+            pacQuery.AgregarPaciente(paciente);
+        }
+
+        // PUT api/<PacienteController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Paciente paciente)
+        {
+            pacQuery.ActualizarPaciente(id, paciente);
+        }
+
+        // DELETE api/<PacienteController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            pacQuery.InactivarPaciente(id);
+        }
+    }
+}
