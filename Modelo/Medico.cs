d5c08db2c4d9be1e191d8b64fbcf3506b4fb4299
rpcorralesvargas-maker <rpcorralesvargas@gmail.com>
2025-08-20 21:33:35 -0600

    Add files via upload

---
+++ Modelo/Medico.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Final.Modelo
+{
+    public class Medico : Persona
+    {
+        public string Especialidad { get; set; }
+        public DateTime? Horario_consulta { get; set; }
+
+        public Medico(int id, string nombre, string cedula, string especialidad, int? telefono, string correo, DateTime? horario_consulta, int? estado)
+            : base (id, nombre, cedula, estado, telefono, correo)
+        {
+            Especialidad = especialidad;
+            Horario_consulta = horario_consulta;
+        }
+
+    }
+}
---
+++ Modelo/Paciente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Final.Modelo
+{
+    public class Paciente : Persona
+    {
+        public string Genero { get; set; }
+        public string Direccion { get; set; }
+        public DateTime? Fecha_nacimiento { get; set; }
+        public DateTime? Fecha_registro { get; set; }
+
+        public Paciente (int id, string nombre, string cedula, DateTime? fecha_nacimiento, string genero, string direccion, int? telefono, string correo, int? estado, DateTime? fecha_registro)
+            : base (id, nombre, cedula, estado, telefono, correo)
+        {
+            Fecha_nacimiento = fecha_nacimiento;
+            Genero = genero;
+            Direccion = direccion;
+            Fecha_registro = fecha_registro;
+        }
+    }
+}
